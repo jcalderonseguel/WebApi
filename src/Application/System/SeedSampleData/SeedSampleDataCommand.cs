ecca6f2cf43790fa32ce06d6e923697105b3bcab
Javier  Calderon <33453799+jcalderonseguel@users.noreply.github.com>
2020-01-28 19:01:48 +0100

    Seed & BD Automatica

---
+++ src/Application/System/SeedSampleData/SeedSampleDataCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.System.SeedSampleData
+{
+    public class SeedSampleDataCommand : IRequest
+    {
+    }
+    public class SeedSampleDataCommandHandler : IRequestHandler<SeedSampleDataCommand>
+    {
+        private readonly IPersonDbContext _context;
+
+        public SeedSampleDataCommandHandler(IPersonDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(SeedSampleDataCommand request, CancellationToken cancellationToken)
+        {
+            var seeder = new SampleDataSeeder(_context);
+
+            await seeder.SeedAllAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
