4ac7993244dc54aeec419575f9cd1fbd5d6d4ee8
estebanl-lagash <estebanl@lagash.com>
2020-01-29 17:15:40 -0300

    Update 29-01-2020
    Actualizacion y inicio de seeders

---
+++ src/Application/System/SeedSampleData/SeedSampleDataCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.System.SeedSampleData
+{
+    public class SeedSampleDataCommand : IRequest
+    {
+    }
+    public class SeedSampleDataCommandHandler : IRequestHandler<SeedSampleDataCommand>
+    {
+        private readonly IPersonDbContext _context;
+
+        public SeedSampleDataCommandHandler(IPersonDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(SeedSampleDataCommand request, CancellationToken cancellationToken)
+        {
+            var seeder = new SampleDataSeeder(_context);
+
+            await seeder.SeedAllAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
