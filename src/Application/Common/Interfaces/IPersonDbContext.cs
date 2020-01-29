64e2e3acc8155d474e6ae1aff175e18df6701f38
melinag <melinag@lagash.com>
2020-01-29 09:15:22 -0300

    se añade personDbContext e IpersonDbContext

---
+++ src/Application/Common/Interfaces/IPersonDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IPersonDbContext
+    {
+        DbSet< Person > Persons { get; set; }
+        DbSet<Address> Address { get; set; }
+        DbSet<Country> Countries { get; set; }
+        DbSet<Gender> Genders { get; set; }
+        DbSet<Email> Emails { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Persistance/PersonDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+
+    public class PersonDbContext : DbContext, IPersonDbContext
+    {
+
+        public PersonDbContext(DbContextOptions<PersonDbContext> options)
+           : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasOne(d => d.Person)
+                    .WithMany(p => p.Address)
+                    .HasForeignKey(d => d.PersonId);
+
+            });
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PersonDbContext).Assembly);
+
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Address> Address { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Gender> Genders { get; set; }
+        public virtual DbSet<Email> Emails { get; set; }
+    }
+}
