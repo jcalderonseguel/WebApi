bc4255a9dd5655e01afac37c0577ade0c2e01476
jeremya-lagash <jeremya@lagash.com>
2020-02-07 15:22:46 -0300

    q

---
+++ src/Application/System/SeedSampleDataCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.System
+{
+    public class SeedSampleDataCommand : IRequest
+    {
+    }
+
+    public class SeedSampleDataCommnandHandler : IRequestHandler<SeedSampleDataCommand>
+    {
+        private readonly IPersonDbContext _context;
+
+        public SeedSampleDataCommnandHandler(IPersonDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(SeedSampleDataCommand request, CancellationToken cancellationToken)
+        {
+            var seeder = new SampleDataSeeder(_context);
+            await seeder.SeedAllAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
