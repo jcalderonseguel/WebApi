a45bd45fdd607bdc5d2e1768393f391edc90796e
jeremya-lagash <jeremya@lagash.com>
2020-02-07 15:16:13 -0300

    creo que ahora si arregle el get de genders :)

--- src/Domain/Entities/Email.cs
+++ src/Domain/Entities/Email.cs
@@ -8,7 +8,7 @@ namespace Domain.Entities
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public int PersonId { get; set; }
+        public int? PersonId { get; set; }
 
         public virtual Person Person { get; set; }
     }
